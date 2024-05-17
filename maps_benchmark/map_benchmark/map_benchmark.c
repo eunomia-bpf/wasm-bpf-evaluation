d0bc25e4c02299a3f1504499030e0974b0982a5e
officeyutong <yt.xyxx@gmail.com>
2024-05-17 23:13:03 +0800

    update

--- maps_benchmark/map_benchmark/map_benchmark.c
+++ maps_benchmark/map_benchmark/map_benchmark.c
@@ -18,6 +18,8 @@
 
 #define NANO_SECOND_TO_TEST ((uint64_t)1000 * 1000 * 1000 * 3)
 
+#define TEST_COUNT 1000000
+
 static uint64_t get_timestamp() {
   struct timespec ts;
   clock_gettime(CLOCK_REALTIME, &ts);
@@ -42,16 +44,16 @@ int main(int argc, char *argv[]) {
     int64_t value = (i << 32) | i;
     bpf_map_update_elem(mapfd, &i, &value, 0);
   }
-  uint64_t count = 0;
+  // uint64_t count = 0;
   uint64_t start = get_timestamp();
-  while (get_timestamp() - start < NANO_SECOND_TO_TEST) {
+  for (int i = 1; i <= 1000000; i++) {
     int64_t key = 10;
     int64_t value_out;
     bpf_map_lookup_elem(mapfd, &key, &value_out);
-    count++;
+    // count++;
   }
   uint64_t time_elapsed = get_timestamp() - start;
-  printf("%" PRIu64 " %" PRIu64 "\n", time_elapsed, count);
+  printf("%" PRIu64 " %" PRIu64 "\n", time_elapsed, (uint64_t)TEST_COUNT);
 cleanup:
   map_benchmark_bpf__destroy(skel);
   return err < 0 ? -err : 0;
--- maps_benchmark/run.py
+++ maps_benchmark/run.py
@@ -10,7 +10,7 @@ ASSETS_DIR = WORK_DIR/"assets"
 
 WASM_BPF = WORK_DIR.parent/"assets"/"wasm-bpf"
 
-DOCKER_IMAGE = "704b7e51cbc6"
+DOCKER_IMAGE = "5a03a529e80b"
 FLAME_GRAPH_ROOT = pathlib.Path("/root/FlameGraph")
 
 
@@ -38,7 +38,7 @@ def run_simple(cmdline: List[str], perf_data_name: Union[str, None] = None, star
     print(lines)
     data_line = lines[-1]
     time, count = (float(x) for x in data_line.strip().split())
-    return count/(time/1e6)
+    return time/count
 
 
 def generate_statistics(data: List[float]):
@@ -70,24 +70,24 @@ def main():
     for i in range(10):
         native_result_with_perf.append(run_simple(
             [str(ASSETS_DIR/"map_benchmark")], WORK_DIR/"result"/f"native{i}.perf", False))
-        native_result_without_perf.append(run_simple(
-            [str(ASSETS_DIR/"map_benchmark")]))
+        # native_result_without_perf.append(run_simple(
+        #     [str(ASSETS_DIR/"map_benchmark")]))
     wasm_result_with_perf = []
     wasm_result_without_perf = []
     for i in range(10):
         wasm_result_with_perf.append(run_simple(
             [WASM_BPF, str(ASSETS_DIR/"map_benchmark.wasm")], WORK_DIR/"result"/f"wasm{i}.perf"))
-        wasm_result_without_perf.append(run_simple(
-            [WASM_BPF, str(ASSETS_DIR/"map_benchmark.wasm")], None))
+        # wasm_result_without_perf.append(run_simple(
+        #     [WASM_BPF, str(ASSETS_DIR/"map_benchmark.wasm")], None))
     docker_result = []
     for i in range(10):
         docker_result.append(
             run_simple(["docker", "run", "--privileged", "-v", "/sys:/sys", DOCKER_IMAGE], None))
     result = {
         "native_perf": generate_statistics(native_result_with_perf),
-        "native_no_perf": generate_statistics(native_result_without_perf),
+        # "native_no_perf": generate_statistics(native_result_without_perf),
         "wasm_perf": generate_statistics(wasm_result_with_perf),
-        "wasm_no_perf": generate_statistics(wasm_result_without_perf),
+        # "wasm_no_perf": generate_statistics(wasm_result_without_perf),
         "docker": generate_statistics(docker_result)
     }
     print(result)
